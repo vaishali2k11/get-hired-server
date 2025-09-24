3100e552e595742d2178c2a4d0383f50afea2ee8
Rahul Rathor <rathore.vaishali1011@gmail.com>
2025-09-24 09:59:39 +0530

    upate

--- src/database.ts
+++ src/database.ts
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 console.log(' process.env.DATABASE_USERNAME:',  process.env.DATABASE_USERNAME)
+console.log('process.env.DATABASE_HOST:', process.env.DATABASE_HOST)
 const db = new Pool({
   user: process.env.DATABASE_USERNAME,
   host: process.env.DATABASE_HOST,
